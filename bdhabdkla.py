14ccabee5f8978287d2b7e9bd4ea1d1ea8186b8b
grigory karpunin <143996987+sae1012@users.noreply.github.com>
2024-03-26 20:59:04 +0300

    Update bdhabdkla.py

--- bdhabdkla.py
+++ bdhabdkla.py
@@ -2,6 +2,7 @@ import telebot
 import random
 
 
+
 token = "7043472916:AAE4VE3JPaLS46uSruhmleSRpguMjEK7E4k"
 bot = telebot.TeleBot(token=token)
 notes = {}
@@ -37,12 +38,11 @@ def compare(message):
     if num_random > int(message.text):
         more = bot.send_message(user_id, "Больше!")
         bot.register_next_step_handler(more, compare)
-    elif num_random < int(message.text):
+    if num_random < int(message.text):
         less = bot.send_message(user_id, "Меньше!")
         bot.register_next_step_handler(less, compare)
-    elif num_random == int(message.text):         
-        msg1 = bot.send_message(user_id, "Вам удалось отгадать число,это в самом деле,", num_random)
+    if num_random == int(message.text):         
+        msg1 = bot.send_message(user_id, "Вам удалось отгадать число, это в самом деле, " + str(num_random))
         bot.register_next_step_handler(msg1, priv)
-
 
 bot.polling(none_stop=True)
